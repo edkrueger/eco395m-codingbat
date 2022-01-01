eddbdc18eef2106e63a555c73470ef5ae3c67cea
edkrueger <edkrueger@gmail.com>
2022-01-01 15:51:10 -0600

    Normalize quotes in doc strings.

--- warmup-1.py
+++ warmup-1.py
@@ -98,9 +98,9 @@ def not_string(str):
     """
     Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
 
-    not_string("candy") → "not candy"
-    not_string("x") → "not x"
-    not_string("not bad") → "not bad"
+    not_string('candy') → 'not candy'
+    not_string('x') → 'not x'
+    not_string('not bad') → 'not bad'
     """
     return
 
